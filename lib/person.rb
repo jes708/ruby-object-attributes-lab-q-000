b42e48e47b2c94b9ece5e0c4e66a316f9ae5822e
jes708 <jonschwarz@nyu.edu>
2016-01-24 21:12:00 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+
+=begin
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+=end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+end
